2dbd71e6ced5c94355a404f4184eb96711fe39a0
Kantselyarchik Mihail <m.kantselyarchik@itransition.com>
2020-07-07 18:01:42 +0300

    Add user proto file. Add scripts for generation protobuf go file, gateway file and swagger json for user service and user go file for api client service and generate them.

--- bin/protogen.sh
+++ bin/protogen.sh
@@ -14,5 +14,19 @@ protoc -I/usr/local/include -I. \
   ./services/portfolio/protobuf/portfolio.proto \
   --go_out=plugins=grpc:services/api-client/services/portfolio/protobuf
 
+echo "Generation protobuf go file, gateway file and swagger json for user service"
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.14.6/third_party/googleapis \
+  ./services/user/protobuf/user.proto \
+  --grpc-gateway_out=logtostderr=true:services/api-client \
+  --swagger_out=logtostderr=true:swagger-ui/ \
+  --go_out=plugins=grpc:services/user/protobuf
+
+echo "Generation protobuf user go file for api client service"
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.14.6/third_party/googleapis \
+  ./services/portfolio/protobuf/portfolio.proto \
+  --go_out=plugins=grpc:services/api-client/services/portfolio/protobuf
+
 
 
